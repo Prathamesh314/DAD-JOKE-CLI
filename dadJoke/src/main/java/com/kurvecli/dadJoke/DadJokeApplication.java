215135e8866731553d22ebe150004c8e89c21e88
Prathamesh314 <124865398+Prathamesh314@users.noreply.github.com>
2023-10-02 12:07:50 +0530

    Initialized spring boot application

---
+++ dadJoke/src/main/java/com/kurvecli/dadJoke/DadJokeApplication.java
@@ -0,0 +1,13 @@
+package com.kurvecli.dadJoke;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DadJokeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DadJokeApplication.class, args);
+	}
+
+}
---
+++ dadJoke/src/test/java/com/kurvecli/dadJoke/DadJokeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kurvecli.dadJoke;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DadJokeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
